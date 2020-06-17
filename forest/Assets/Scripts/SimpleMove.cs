3f3af2114e7c22fffe120906f12fff4d77a0b755
Link <timelyeee@gmail.com>
2020-06-17 06:27:56 +0200

    LOL simple move

---
+++ forest/Assets/Scripts/SimpleMove.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Creates wandering behaviour for a CharacterController.
+/// </summary>
+[RequireComponent(typeof(CharacterController))]
+public class SimpleMove : MonoBehaviour
+{
+    public float speed = 5;
+    public float directionChangeInterval = 1;
+    public float maxHeadingChange = 30;
+    private Animator anim;
+
+    private Transform player;
+
+    CharacterController controller;
+    float heading;
+    Vector3 targetRotation;
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    void Awake()
+    {
+        // Set random initial rotation
+        heading = Random.Range(0, 360);
+        transform.eulerAngles = new Vector3(0, heading, 0);
+
+        StartCoroutine(NewHeading());
+
+
+
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, player.position) < 50f)
+        {
+            if (anim.GetBool("Walk"))
+            {
+                anim.SetBool("Walk", false);
+                anim.SetBool("Run", true);
+            }
+            
+            var forward = transform.TransformDirection((player.position - transform.position).normalized);
+            controller.SimpleMove(forward * speed*2);
+        }
+        else
+        {
+            if (!anim.GetBool("Walk"))
+            {
+                anim.SetBool("Walk", true);
+                anim.SetBool("Run", false);
+            }
+            transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, targetRotation, Time.deltaTime * directionChangeInterval);
+            
+            var forward = transform.TransformDirection(Vector3.forward);
+            controller.SimpleMove(forward * speed);
+        }
+        
+    }
+
+    /// <summary>
+    /// Repeatedly calculates a new direction to move towards.
+    /// Use this instead of MonoBehaviour.InvokeRepeating so that the interval can be changed at runtime.
+    /// </summary>
+    IEnumerator NewHeading()
+    {
+        while (true)
+        {
+            NewHeadingRoutine();
+            yield return new WaitForSeconds(directionChangeInterval);
+        }
+    }
+
+    /// <summary>
+    /// Calculates a new direction to move towards.
+    /// </summary>
+    void NewHeadingRoutine()
+    {
+        var floor = Mathf.Clamp(heading - maxHeadingChange, 0, 360);
+        var ceil = Mathf.Clamp(heading + maxHeadingChange, 0, 360);
+        heading = Random.Range(floor, ceil);
+        targetRotation = new Vector3(0, heading, 0);
+    }
+}
+
